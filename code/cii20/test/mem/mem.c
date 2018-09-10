4997bee70c2b978554a98dc565f39f9c12dad9f9
ren <1101553279@qq.com>
2018-09-10 12:11:23 +0000

    mem.c

--- code/cii20/test/mem/mem.c
+++ code/cii20/test/mem/mem.c
@@ -4,10 +4,13 @@ static char rcsid[] = "$Id: mem.c 6 2007-01-22 00:45:22Z drhanson $";
 #include "assert.h"
 #include "except.h"
 #include "mem.h"
+
 const Except_T Mem_Failed = { "Allocation Failed" };
 void *Mem_alloc(long nbytes, const char *file, int line){
 	void *ptr;
+
 	assert(nbytes > 0);
+
 	ptr = malloc(nbytes);
 	if (ptr == NULL)
 		{
@@ -18,11 +21,15 @@ void *Mem_alloc(long nbytes, const char *file, int line){
 		}
 	return ptr;
 }
+
 void *Mem_calloc(long count, long nbytes,
 	const char *file, int line) {
+
 	void *ptr;
+
 	assert(count > 0);
 	assert(nbytes > 0);
+
 	ptr = calloc(count, nbytes);
 	if (ptr == NULL)
 		{
@@ -33,21 +40,26 @@ void *Mem_calloc(long count, long nbytes,
 		}
 	return ptr;
 }
+
 void Mem_free(void *ptr, const char *file, int line) {
 	if (ptr)
 		free(ptr);
 }
+
 void *Mem_resize(void *ptr, long nbytes,
 	const char *file, int line) {
+
 	assert(ptr);
 	assert(nbytes > 0);
+
 	ptr = realloc(ptr, nbytes);
 	if (ptr == NULL)
-		{
-			if (file == NULL)
-				RAISE(Mem_Failed);
-			else
-				Except_raise(&Mem_Failed, file, line);
-		}
+    {
+        if (file == NULL)
+            RAISE(Mem_Failed);
+        else
+            Except_raise(&Mem_Failed, file, line);
+    }
+
 	return ptr;
 }
